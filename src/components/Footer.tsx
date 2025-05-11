75440fb5014c2fed8a7a73ebe24250f1ca11c2bd
Fahad <codesfahad@gmail.com>
2025-05-12 02:42:48 +0500

    HomePage Revamped Completed.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,14 @@
-
-import { Link } from "react-router-dom";
-import { Mail, Github, Linkedin } from "lucide-react";
-
 const Footer = () => {
   return (
-    <footer className="py-6">
+    <footer className="py-6 border-t border-neutral-700/80">
       <div className="container-custom flex gap-2 flex-col items-center justify-center">
               <span className="font-heading font-bold text-lg">Buildfolio.io</span>
             <p className="text-muted-foreground">
               MADE WITH ❤️ BY <span className="font-bold">MUHAMMAD FAHAD  </span>
+            </p>
+
+            <p className="text-muted-foreground">
+              © {new Date().getFullYear()} Buildfolio. All rights reserved.
             </p>
       </div>
     </footer>
--- src/components/GetStartedCTA.tsx
+++ src/components/GetStartedCTA.tsx
@@ -5,22 +5,21 @@ import { ChevronRight } from "lucide-react";
 
 const GetStartedCTA = () => {
   return (
-    <div className="section bg-gradient-to-r from-blue-500 to-purple-500 text-white">
+    <div className="section text-white">
       <div className="container-custom text-center">
         <h2 className="text-3xl md:text-4xl font-bold mb-6">
           Ready to showcase your skills to the world?
         </h2>
         <p className="text-lg md:text-xl mb-10 max-w-2xl mx-auto opacity-90">
-          Join thousands of professionals who have created stunning portfolios with our easy-to-use platform.
+          Join thousands of developers building standout portfolios <br/> no coding, no setup, just results.
         </p>
         <Button 
           size="lg" 
-          variant="secondary" 
           asChild
-          className="hover-lift font-semibold px-8 py-6 text-base"
+          className="font-semibold px-8 py-6 text-base bg-white text-black hover:bg-transparent border-2 border-white hover:text-white transition duration-300"
         >
           <Link to="/builder" className="flex items-center gap-2">
-            Build Your Portfolio <ChevronRight size={18} />
+            Create Portfolio Now <ChevronRight size={18} />
           </Link>
         </Button>
       </div>
